7daec961a18ca0aebe4c3a0ae084d11bac067128
Kristoff <60750557+octoDragon@users.noreply.github.com>
2021-05-07 20:33:38 -0700

    Water Collider + Player Rotation

--- O-Zone-Project/Assets/Scripts/PlayerComponents.cs
+++ O-Zone-Project/Assets/Scripts/PlayerComponents.cs
@@ -9,6 +9,9 @@ namespace Player
         static PlayerControls PControls;
         static Animator PAnimator;
         static PlayerStats PStats;
+        static Transform PTransform;
+        static GameObject PGameObj;
+        static Rigidbody2D PRigidBody;
 
         /// <summary>
         /// set each variable to the component on the Octo
@@ -18,12 +21,18 @@ namespace Player
             PControls = gameObject.GetComponent<PlayerControls>();
             //PAnimator = gameObject.GetComponent<Animator>();
             PStats = gameObject.GetComponent<PlayerStats>();
+            PTransform = gameObject.transform;
+            PGameObj = gameObject;
+            PRigidBody = gameObject.GetComponent<Rigidbody2D>();
         }
 
         /// <summary>
         /// //get methods, used for accessing the Octo's components
         /// </summary>
         public static PlayerControls GetPControls => PControls;
+        public static Transform GetPTransform => PTransform;
+        public static GameObject GetPGameObject => PGameObj;
+        public static Rigidbody2D GetPRigidBody => PRigidBody;
         //public static Animator GetPAnimator => PAnimator;
         public static PlayerStats GetPStats => PStats;
 
--- O-Zone-Project/Assets/Scripts/PlayerStats.cs
+++ O-Zone-Project/Assets/Scripts/PlayerStats.cs
@@ -4,7 +4,34 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour
 {
+    //Movement + Physics values
+    [SerializeField] float RotationSpeed = 8f;
+    [SerializeField] float MovementSpeed = 8f;
+    //SceneLogic
     int round_win_count;
+    bool InWater = false;
+    
 
     public void RoundWin() { round_win_count++; }
+
+    //rotation
+    public float GetRotationSpeed => RotationSpeed;
+    public float GetMovementSpeed => MovementSpeed;
+    public float GetAngle(Vector2 me, Vector2 target)
+    {
+        //math for rotating spoon
+        return Mathf.Atan2(target.y - me.y, target.x - me.x)
+            * (180 / Mathf.PI);
+    }
+
+    //movement
+    
+
+    //terrain
+    public bool GetInWater => InWater;
+
+    public void SetInWater(bool change)
+    {
+        InWater = change;
+    }
 }
