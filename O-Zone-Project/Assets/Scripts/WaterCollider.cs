38975e0c53d8cb22a5bfbd35145fe3544364117b
Cade Midyett <cademidyett@gmail.com>
2021-05-07 21:44:46 -0700

    Added water gravity

--- O-Zone-Project/Assets/Scripts/WaterCollider.cs
+++ O-Zone-Project/Assets/Scripts/WaterCollider.cs
@@ -6,17 +6,21 @@ public class WaterCollider : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" || collision.tag == "Projectile")
         {
             collision.gameObject.GetComponent<PlayerStats>().SetInWater(true);
         }
+
+        collision.attachedRigidbody.gravityScale = 0;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.tag == "Player" || collision.tag == "Projectile")
         {
             collision.gameObject.GetComponent<PlayerStats>().SetInWater(false);
         }
+
+        collision.attachedRigidbody.gravityScale = 1;
     }
 }
