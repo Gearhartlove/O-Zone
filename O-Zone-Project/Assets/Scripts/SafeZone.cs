9a3eadb584f8a3b5caa1fefb958307e743ef5a87
Cade Midyett <cademidyett@gmail.com>
2021-05-09 21:31:19 -0700

    :shaved_ice: Started implementing screen shaking

--- O-Zone-Project/Assets/Scripts/SafeZone.cs
+++ O-Zone-Project/Assets/Scripts/SafeZone.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SafeZone : MonoBehaviour
 {
+    [SerializeField] GameObject DeathParticles;
+    [SerializeField] Camera MainCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,15 @@ public class SafeZone : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<PlayerStats>().Damage(1000);
+
+            Vector3 DeathParticlePosition = collision.transform.position;
+            Vector3 CameraCenter = MainCamera.transform.position;
+            Vector3 collisionPosition = collision.transform.position;
+            float DeathParticleRotation = Vector3.Angle(collisionPosition, CameraCenter);
+            Debug.Log(DeathParticleRotation);
+            Quaternion DeathParticleQuaternion = Quaternion.Euler(0, 0, DeathParticleRotation);
+            Instantiate(DeathParticles, collision.transform.position, DeathParticleQuaternion);
+
         } else if (collision.CompareTag("Projectile"))
         {
             collision.GetComponent<Fruit>().DestroyFruit();
