f9a2095fe7f136d2f5161b2cf34e5c8559d9835d
Kristoff <60750557+octoDragon@users.noreply.github.com>
2021-06-03 12:08:49 -0800

    Countdown Script More Readable

--- O-Zone-Project/Assets/Countdown.cs
+++ O-Zone-Project/Assets/Countdown.cs
@@ -5,17 +5,20 @@ using TMPro;
 
 public class Countdown : MonoBehaviour
 {
+    //Components
     private static Countdown countdown;
     static Animator animator;
     static CanvasGroup canvas_group;
-    static float countdown_delay = 3f;
-    private float TempTimer = 0f;
-    public static float GetCountdown_Delay => countdown_delay;
+    //Variables
     public static bool StartTimer = false;
+    static float countdown_delay = 3f;
+    public static float GetCountdown_Delay => countdown_delay;
+    private float TempTimer = 0f;
     [SerializeField] TMPro.TextMeshProUGUI countdown_text;
 
     private void Awake()
     {
+        //because static, need to prevent from assigning again
         DontDestroyOnLoad(gameObject);
         if (countdown == null) countdown = GetComponent<Countdown>();
         else { Destroy(gameObject); }
@@ -25,10 +28,11 @@ public class Countdown : MonoBehaviour
 
     private void Update()
     {
+        //only run when the Countdown timer has started
+        //(after the scoreboard is hidden)
         if (StartTimer)
         {
             TempTimer += Time.deltaTime;
-            //I'm worried that it will skip when I don't want it too
             if (TempTimer >= 3f)
             {
                 //hide the UI
