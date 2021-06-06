3660f6ea0d81c567eb0ce9982d51a387814c1394
Kristoff <60750557+octoDragon@users.noreply.github.com>
2021-06-06 08:39:10 -0800

    Lists of Each Stages
    Seperate and Load Stages based on their names. They are seperated into their own respective STAGE_TYPES found in StageManager. Utilizes the @"Assets/Scenes" Directory to read the files and then find the Scene Names, and then puts the names into a list. To load a stage, simply call "SceneManager.LoadScene(beach_list[index]);
    This loads a string based on the string provided. :100:

--- O-Zone-Project/Assets/Scripts/StageManager.cs
+++ O-Zone-Project/Assets/Scripts/StageManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
+using System;
 
 public class StageManager : MonoBehaviour
 {
@@ -10,49 +12,104 @@ public class StageManager : MonoBehaviour
     //??try to loop through the SceneBuilder
     //and find how many of each stage -> not hardcoded
 
-    private List<Stage> stages_list;
+    //Note: string[] lines = File.ReadAllLines(@"Assets/StoryBeats/dialogue1.txt");
+
+    private String[] STAGE_TYPES;
+
+    private List<String>[] my_stages; //debugging purposes
+    private List<String> evil_list;
+    private List<String> beach_list;
+    private List<String> rocky_list;
 
     private void Awake()
     {
-        stages_list = new List<Stage>();
-        LoadScenesAsync();
+        //if you add to this list, must update the "DetermineStageType()"
+        //method below
+        STAGE_TYPES = new String[]
+        {
+            "evil",
+            "beach",
+            "rocky"
+        };
+        evil_list =  new List<String>();
+        beach_list = new List<String>();
+        rocky_list = new List<String>();
+
+        my_stages = new List<String>[]
+        {
+            evil_list,
+            beach_list,
+            rocky_list
+        };
+
+        SeperateStages();
+        PrintStages();
+
+        //delete after
+        SceneManager.LoadScene(evil_list[0]);
     }
 
     private int GetTotalSceneCount => SceneManager.sceneCountInBuildSettings;
 
-    public void LoadScenesAsync()
+    private void SeperateStages()
     {
-        StartCoroutine(LoopThroughScenes());
-        Debug.Log("scenes looped");
+        DirectoryInfo DI = new DirectoryInfo(@"Assets/Scenes");
+        try
+        {
+            // Determine whether the directory exists.
+            if (DI.Exists)
+            {
+                try
+                {
+                    FileInfo[] FI = DI.GetFiles();
+                    // Indicate that the directory already exists.
+                    for (int i = 0; i < FI.Length; i++)
+                    {
+                        //if not a .meta file
+                        if (!FI[i].Name.EndsWith(".meta"))
+                        {
+                            //removes ".unity" from end of the stage
+                            //important for loading the stages later
+                            string stage_name = FI[i].Name.
+                                Substring(0, FI[i].Name.Length - 6);
+                            
+                            //call method with switch statment for stage types
+                            DetermineStageType(stage_name);
+                        }
+
+                    }
+                    return;
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Debug.Log("String out of bounds:\n" +
+                        e.ToString());
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log("The process failed: " + e.ToString());
+        }
     }
 
-    private IEnumerator LoopThroughScenes()
+    private void DetermineStageType(string stage_name)
     {
-        yield return null;
+        if (stage_name.StartsWith("evil")) evil_list.Add(stage_name);
+        else if (stage_name.StartsWith("beach")) beach_list.Add(stage_name);
+        else if (stage_name.StartsWith("rocky")) rocky_list.Add(stage_name);
+    }
 
-        //Debug.Log("Total Scene Count: " + GetTotalSceneCount);
-        //i = 1 to not load starting scene 2x (freezes application)
-        for (int i = 1; i < GetTotalSceneCount; i++)
+    //Debugging purposes
+    private void PrintStages()
+    {
+        foreach (List<String> list in my_stages)
         {
-            AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(i);
-            asyncOperation.allowSceneActivation = false;
-            Debug.Log("Pro :" + asyncOperation.progress);
-            //When the load is still in progress, output the Text and progress bar
-            while (!asyncOperation.isDone)
+            foreach (String s in list)
             {
-                //Output the current progress
-                Debug.Log("Loading progress: " + (asyncOperation.progress * 100) + "%");
-
-                // Check if the load has finished
-                if (asyncOperation.progress >= 0.9f)
-                {
-                    //Change the Text to show the Scene is ready
-                    Debug.Log("Press the space bar to continue");
-                    //Wait to you press the space key to activate the Scene
-                }
-
-                yield return null;
+                Debug.Log(s);
             }
         }
     }
+
 }
